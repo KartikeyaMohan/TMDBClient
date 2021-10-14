6015dc12ccee8cd82d28040a57766d5abac6c667
Kartikeya Mohan <kartikeyamohan@gmail.com>
2021-10-14 15:01:06 +0530

    Added unit tests

---
+++ app/src/androidTest/java/com/kmx/tmdbclient/data/db/MovieDaoTest.kt
@@ -0,0 +1,65 @@
+package com.kmx.tmdbclient.data.db
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.kmx.tmdbclient.data.models.movieObjects.Movie
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MovieDaoTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var dao: MovieDao
+    private lateinit var database: TMDBDatabase
+
+    @Before
+    fun setUp() {
+        database =
+            Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(),
+                TMDBDatabase::class.java).build()
+        dao = database.movieDao()
+    }
+
+    @Test
+    fun saveMoviesTest() = runBlocking {
+        val movies = listOf(
+            Movie(1, "overview1", "posterPath1", "date1", "title1"),
+            Movie(2, "overview2", "posterPath2", "date2", "title2"),
+            Movie(3, "overview3", "posterPath3", "date3", "title3")
+        )
+        dao.saveMovies(movies)
+
+        val allMovies = dao.getMovies()
+
+        Truth.assertThat(allMovies).isEqualTo(movies)
+    }
+
+    @Test
+    fun deleteMoviesTest() = runBlocking {
+        val movies = listOf(
+            Movie(1, "overview1", "posterPath1", "date1", "title1"),
+            Movie(2, "overview2", "posterPath2", "date2", "title2"),
+            Movie(3, "overview3", "posterPath3", "date3", "title3")
+        )
+        dao.saveMovies(movies)
+        dao.deleteMovies()
+        val allMovies = dao.getMovies()
+
+        Truth.assertThat(allMovies).isEmpty()
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+    }
+}
